55f3169459551cb868e5bf4e2baa6450b0a95149
Rabin Shah <108661077+rabin9087@users.noreply.github.com>
2023-12-30 13:36:28 +1100

    CustomCarousel.js file changes

--- src/components/carousel/CustomCarousel.jsx
+++ src/components/carousel/CustomCarousel.jsx
@@ -10,24 +10,24 @@ export const CustomeCarousel = () => {
       <Carousel.Item>
         <img className="d-block w-100" src={library1} alt="First slide" />
         <Carousel.Caption>
-          <h5>First slide label</h5>
-          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+          {/* <h5>First slide label</h5>
+          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p> */}
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
         <img className="d-block w-100" src={library2} alt="Second slide" />
         <Carousel.Caption>
-          <h5>Second slide label</h5>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+          {/* <h5>Second slide label</h5>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p> */}
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
         <img className="d-block w-100" src={library3} alt="Third slide" />
         <Carousel.Caption>
-          <h5>Third slide label</h5>
+          {/* <h5>Third slide label</h5>
           <p>
             Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-          </p>
+          </p> */}
         </Carousel.Caption>
       </Carousel.Item>
     </Carousel>
